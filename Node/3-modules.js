309ab93ccb5eae01949a9d5be63d2cee7b1c7cbf
cristiantabira <cristian.tabira@yahoo.com>
2024-03-05 00:15:45 +0200

    Done the first part of the tutorial

---
+++ Node/3-modules.js
@@ -0,0 +1,12 @@
+//Modules
+const names=require('./4-namesModule')
+console.log(names)
+
+require(`./7-mind-grenade`)
+
+const data= require(`./6-alternative-flavor`)
+// console.log(data)
+const sayHi=require(`./5-utils`)
+// sayHi("susan")
+// sayHi(names.john)
+// sayHi(names.peter)
---
+++ Node/7-mind-grenade.js
@@ -0,0 +1,9 @@
+const a=5
+const b=10
+
+function sum()
+{
+    console.log(`The sum is:${a+b}`)
+
+}
+sum()
